a81d20c2dbd9d9949c4640a427ab57363121100b
Pranay Kuppa <pkuppa@umd.edu>
2025-08-10 17:45:18 -0400

    Minor tweak

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import status
 from . import model
 from .api.user_routes import router as user_router
 from .api.tracker_routes import router as tracker_router
@@ -9,10 +10,10 @@ app.include_router(tracker_router)
 
 @app.get("/")
 def root():
-    return {"stataus": 200,
+    return {"stataus": status.HTTP_200_OK,
         "message": "Welcome to Amtrak Price Tracker"}
 
 @app.get("/health")
 def heartbeat():
-    return {"status": 200,
+    return {"status": status.HTTP_200_OK,
             "message": "Server is alive"}
